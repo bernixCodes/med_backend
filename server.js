2563f911d2087706c6d4807a808c74ef159cc935
bernixCodes <adjoa.bernie@gmail.com>
2024-02-16 18:08:53 +0000

    cors update

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectedDB = require("./config/db");
 require("dotenv").config();
+const cors = require("cors");
 connectedDB();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors({ allowedHeaders: ["Content-Type", "Authorization"] }));
 app.use(express.json());
 app.use("/api/drugs", require("./routes/drugRoutes"));
 app.use("/api/labs", require("./routes/labRoutes"));
 app.use(errorHandler);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 
 app.listen(port, () => {
   console.log(`server running on ${port}`);
