43466f1e414da902ac34a847156ffb4a5c88f363
bernixCodes <adjoa.bernie@gmail.com>
2024-02-16 17:50:28 +0000

    adding cors

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use("/api/drugs", require("./routes/drugRoutes"));
 app.use("/api/labs", require("./routes/labRoutes"));
 app.use(errorHandler);
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.listen(port, () => {
   console.log(`server running on ${port}`);
 });
